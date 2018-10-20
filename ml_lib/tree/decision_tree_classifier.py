9047f0058dc0ce9dd95474766972f9ac772da904
Denys Fridman <denys.fridman@deepsense.ai>
2018-10-20 12:55:24 +0200

    add gini criterion

--- ml_lib/tree/decision_tree_classifier.py
+++ ml_lib/tree/decision_tree_classifier.py
@@ -5,10 +5,17 @@ from ml_lib.tree.decision_tree import DecisionTree
 from ml_lib.viz_utils import plot_decision_boundary
 
 
-def _compute_entropy(y):
+def _compute_distribution(y):
     n = len(y)
-    ps = [count / n for count in Counter(y).values()]
-    return -sum([p*np.log2(p) for p in ps])
+    return [count / n for count in Counter(y).values()]
+
+
+def _compute_entropy(y):
+    return -sum([p*np.log2(p) for p in _compute_distribution(y)])
+
+
+def _compute_gini(y):
+    return 1 - sum(np.square(_compute_distribution(y)))
 
 
 class DecisionTreeClassifier(DecisionTree):
@@ -29,7 +36,8 @@ class DecisionTreeClassifier(DecisionTree):
 
     def _get_criterion_fn(self, criterion):
         return {
-            'entropy': _compute_entropy
+            'entropy': _compute_entropy,
+            'gini': _compute_gini
         }[criterion]
 
 
@@ -38,8 +46,8 @@ if __name__ == '__main__':
     from sklearn.datasets import make_classification
     from sklearn.metrics import accuracy_score
     X, y = make_classification(n_samples=100, n_features=2, n_informative=1, n_redundant=0,
-                               n_clusters_per_class=1, class_sep=0.5, random_state=17)
-    estimator = DecisionTreeClassifier(max_depth=-1)
+                               n_clusters_per_class=1, class_sep=0.2, random_state=17)
+    estimator = DecisionTreeClassifier(max_depth=5, criterion='gini')
     train_predictions = estimator.fit_predict(X, y)
     print('train_accuracy: {}'.format(accuracy_score(y_true=y, y_pred=train_predictions)))
     plot_decision_boundary(X, y, estimator=estimator, num=100, figsize=(16, 16))
