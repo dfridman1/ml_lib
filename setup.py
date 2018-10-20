b4f10a426754c6d8aeca8821108de2106b8c2142
Denys Fridman <denys.fridman@deepsense.ai>
2018-10-20 11:38:44 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='ml_lib',
+    version='0.0.1',
+    description='A Machine Learning Library',
+    url='https://github.com/dfridman1/ml_lib',
+    packages=find_packages(include=["ml_lib"]),
+    install_requires=[
+    ],
+)
